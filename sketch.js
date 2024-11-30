88d097e6b59c94d78d0008f3831f3a3d1690f737
rnedde <rnedde@gmail.com>
2024-11-30 17:22:40 -0500

    drawings save to firebase

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 
 
+var database;
+
 // Variables for Drawing
 let drawing = [];
 let currentPath = [];
@@ -7,12 +9,29 @@ let isDrawing = false;
 
 function setup() {
     let canvas = createCanvas(400, 400);
+
     canvas.mousePressed(startPath);
     canvas.mouseReleased(endPath);
     canvas.parent('#canvas-container');
 
     let saveButton = select("#save-button");
     saveButton.mousePressed(saveDrawing);
+
+    // Your web app's Firebase configuration
+    const config = {
+        apiKey: "AIzaSyBQTe_Pn-hreXMg3HD-v9ODtmSGfUNMx9w",
+        authDomain: "seeing-time-58600.firebaseapp.com",
+        databaseURL: "https://seeing-time-58600-default-rtdb.firebaseio.com",
+        projectId: "seeing-time-58600",
+        storageBucket: "seeing-time-58600.firebasestorage.app-x",
+        messagingSenderId: "337990844288",
+        appId: "1:337990844288:web:42a0229a238f801c10cb65"
+    };
+
+
+    firebase.initializeApp(config);
+    database = firebase.database();
+
 }
 
 function startPath() {
@@ -48,13 +67,14 @@ function draw() {
 }
 
 function saveDrawing() {
-    const drawingsRef = ref(database, 'drawings');
+    const drawingsRef = database.ref('drawings');  // Reference to the 'drawings' node in the database
     const data = {
         name: "Rev",
         drawing: drawing
     };
+    
 
-    push(drawingsRef, data)
+    drawingsRef.push(data)  // Push the data to the database
         .then((snapshot) => {
             console.log("Drawing saved with key:", snapshot.key);
         })
